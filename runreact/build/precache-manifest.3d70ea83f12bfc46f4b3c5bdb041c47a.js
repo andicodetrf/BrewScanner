227aa6da383482b9c48708a322fb2d18d16dc008
andicodetrf <andrea_lau92@hotmail.com>
2020-09-03 17:32:19 +0800

    remove build from gitignore

---
+++ runreact/build/precache-manifest.3d70ea83f12bfc46f4b3c5bdb041c47a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3951d5e8f868a68f67d1e18749f3b1a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96c2cb3b3fdcab9a9af6",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "32d4ab7da9dc26e1a3ae",
+    "url": "/static/css/main.dc78f3d6.chunk.css"
+  },
+  {
+    "revision": "96c2cb3b3fdcab9a9af6",
+    "url": "/static/js/2.e4e19e71.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "/static/js/2.e4e19e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32d4ab7da9dc26e1a3ae",
+    "url": "/static/js/main.ecd90d8d.chunk.js"
+  },
+  {
+    "revision": "7929fccfaf4308c8e395",
+    "url": "/static/js/runtime-main.da60d5b1.js"
+  },
+  {
+    "revision": "c4d1515dbaf99cc1f9ae2fdf8bd1ee4e",
+    "url": "/static/media/beercheers.c4d1515d.jpg"
+  },
+  {
+    "revision": "1c5b124d8320f7c2b13a40b9cba0361c",
+    "url": "/static/media/bottlecheers.1c5b124d.png"
+  }
+]);
